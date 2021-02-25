9e252e0e9da177f89dca2383c7f483371b026415
Harshee-Pitroda <73131499+Harshee-Pitroda@users.noreply.github.com>
2021-02-25 20:00:52 +0530

    add a person

---
+++ js/choosepersonjs.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+    $('h1').mousemove(function(e){
+      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+      $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+    });
+ });
+ 
